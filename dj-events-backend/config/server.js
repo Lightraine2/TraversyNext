1d67362914e18eaedf158e8de9dd5a1425156c89
Lightraine2 <laurencedale1986@gmail.com>
2021-04-23 11:13:37 +0100

    added strapi backend

---
+++ dj-events-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'af6e42af483e992b7e86919b019bb7ad'),
+    },
+  },
+});
---
+++ dj-events-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '42f8e020-236a-49e0-bb41-945e7866976a'
+};
