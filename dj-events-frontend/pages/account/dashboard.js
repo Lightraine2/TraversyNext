e01a3d63b260f8ecbfe60c62d0875f090551ddb4
Lightraine2 <laurencedale1986@gmail.com>
2021-04-30 08:12:08 +0100

    to 44

--- dj-events-frontend/pages/account/dashboard.js
+++ dj-events-frontend/pages/account/dashboard.js
@@ -1,9 +1,9 @@
 import { parseCookies } from '@/helpers/index'
 import { useRouter } from 'next/router'
 import Layout from '@/components/Layout'
-//import DashboardEvent from '@/components/DashboardEvent'
+import DashboardEvent from '@/components/DashboardEvent'
 import { API_URL } from '@/config/index'
-//import styles from '@/styles/Dashboard.module.css'
+import styles from '@/styles/Dashboard.module.css'
 
 export default function DashboardPage({ events, token }) {
   const router = useRouter()
